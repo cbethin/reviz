1416b5940860e5f21d4d9f5c81ab1aeec60c14e6
Charles Bethin <bethin.charles@gmail.com>
2023-07-07 20:35:28 -0400

    get rid of next

---
+++ example/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Button from './components/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button text='Click Me'/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
