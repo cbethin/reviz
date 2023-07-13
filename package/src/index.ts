b1462c5b51f2226c495397f352cff551f15d34ed
Charles Bethin <bethin.charles@gmail.com>
2023-07-13 17:30:05 -0400

    try letting playwright take a screenshot

--- package/src/index.ts
+++ package/src/index.ts
@@ -14,6 +14,7 @@ import imageComparison from './imageComparison';
 import chalk from 'chalk'
 import resetBuilds from './utils/resetBuilds'
 import runDevServer from './runDevServer'
+import openAndScreenshotAStory from './playwright'
 
 const argv = yargs(hideBin(process.argv))
     .command({
@@ -91,6 +92,9 @@ const argv = yargs(hideBin(process.argv))
     .help()
     .argv as Args
 
+openAndScreenshotAStory()
+.then(() => console.log('Done'))
+
 // Accept a current build by moving the current folder to the main folder
 if (argv.accept) {
     if (!fs.existsSync('.reviz/current')) {
@@ -119,7 +123,7 @@ if (argv.accept) {
 
 if (argv.clear) {
     resetBuilds()
-    process.exit()
+    // process.exit()
 }
 
 if (argv['comparisons-only']) {
---
+++ package/src/playwright/index.ts
@@ -0,0 +1,23 @@
+import { chromium, devices } from "playwright";
+
+async function openAndScreenshotAStory() {
+    // Setup
+    const browser = await chromium.launch({
+        headless: false
+    });
+    const context = await browser.newContext(devices['Desktop Chrome'])
+    const page = await context.newPage();
+
+    // The actual interesting bit
+    await context.route('**.jpg', route => route.abort());
+    await page.goto('http://localhost:6006/iframe.html?viewMode=story&id=components-button--another-one');
+    await page.waitForSelector('#storybook-root')
+
+    await page.screenshot({ path: '.reviz/the-screenshot.png', fullPage: true, scale: 'css' })
+
+    // Teardown
+    await context.close()
+    await browser.close()
+}
+
+export default openAndScreenshotAStory
